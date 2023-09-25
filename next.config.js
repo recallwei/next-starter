983776b94c060055c3116a353c56a9a6861a98ea
Bruce <recall4056@gmail.com>
2023-09-25 17:03:52 +0800

    feat: init project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en'
+  }
+}
 
 module.exports = nextConfig
